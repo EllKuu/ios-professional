a4d9f949364d57bd1b069cb0b3b10204ef26fbc2
elliott kung <elliott7506@rogers.com>
2022-03-05 08:50:20 -0500

    refactor: create generic onboarding viewcontroller
    This commit adds a generic onboarding viewcontroller that can be reused across multiple onboarding screens.

--- Bankey/Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -11,26 +11,41 @@ class OnboardingViewController: UIViewController{
     let stackView = UIStackView()
     let imageView = UIImageView()
     let label = UILabel()
+    let heroImageName: String
+    let titleText: String
     
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
         layout()
     }
+    
+    init(heroImageName: String, titleText: String){
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 extension OnboardingViewController{
     func style(){
+        view.backgroundColor = .systemBackground
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 20
         
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
         
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like tou are back in 1989."
+        label.text = titleText
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.textAlignment = .center
         label.adjustsFontForContentSizeCategory = true
